80280f64248b63ed5a2722c45eac11f3fc85586e
PraneethJain <praneethjain005@gmail.com>
2022-04-19 22:29:29 +0530

    Only downloading when not already added

--- src/main.py
+++ src/main.py
@@ -13,17 +13,19 @@ class Instance:
         self.edge_options.add_argument("headless")
         self.edge_options.add_argument("disable-gpu")
         self.edge_options.add_argument("--log-level=3")
-        self.edge_options.add_experimental_option('excludeSwitches', ['enable-logging'])
+        self.edge_options.add_experimental_option("excludeSwitches", ["enable-logging"])
         self.service = Service("msedgedriver.exe")
         self.driver = WebDriver(service=self.service, options=self.edge_options)
         self.initialize()
         self.datas = self.get_all_pages()
+        self.added_now = []
 
     def initialize(self):
         with open("links.txt") as f:
-            links = f.readlines()
-        self.links = [ele.strip() for ele in links]
-        self.names = [link[29:-2].replace("-", " ").capitalize() for link in links]
+            self.links = [ele.strip() for ele in f.readlines()]
+        with open("magnets_added.txt") as f:
+            self.already_added = [ele.strip() for ele in f.readlines()]
+        self.names = [link[29:-2].replace("-", " ").capitalize() for link in self.links]
 
     def get_all_pages(self):
         datas = {}
@@ -48,11 +50,12 @@ class Instance:
             )
         return data
 
-    @staticmethod
-    def start_torrent(magnet: str, name: str) -> None:
+    def start_torrent(self, magnet: str, name: str) -> None:
         qb = Client("http://127.0.0.1:8080/")
         qb.login("admin", "adminadmin")
         qb.download_from_link(magnet, savepath=f"D:\Anime\{name}")
+        self.added_now.append(magnet)
+        
 
     def quit(self):
         self.driver.quit()
@@ -60,7 +63,11 @@ class Instance:
     def download_all(self):
         for name, values in track(self.datas.items(), "Initializing downloads"):
             for episode in values:
-                self.start_torrent(episode["magnet"], name)
+                if episode["magnet"] not in self.already_added:
+                    self.start_torrent(episode["magnet"], name)
+        with open("magnets_added.txt", "a") as f:
+            for m in self.added_now:
+                f.write(f"{m}\n")
         print("Completed!")
 
 
