b80fdfd8f4d03616cc8bcc8d7b16c687e13b2413
PraneethJain <praneethjain005@gmail.com>
2022-06-01 12:01:13 +0530

    Added more arguments

--- src/main.py
+++ src/main.py
@@ -4,7 +4,11 @@ import sys
 if __name__ == '__main__':
     manager = Manager()
     for arg in sys.argv:
-        if arg=='dl':
+        if arg=='download' or arg=='dl':
             manager.update()
-        if arg=='play':
+        if arg=='play' or arg=='watch':
             manager.play('test')
+        if arg=='watchlist' or arg=='wl':
+            manager.watchlist()
+        if arg=='test':
+            manager.test()
