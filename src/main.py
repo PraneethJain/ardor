8c57ec68b9aa754f3df85b69e9c4df4ca4981ac9
PraneethJain <praneethjain005@gmail.com>
2022-06-01 13:31:26 +0530

    Added watchlist commands

--- src/main.py
+++ src/main.py
@@ -3,12 +3,18 @@ import sys
 
 if __name__ == '__main__':
     manager = Manager()
-    for arg in sys.argv:
+    for i,arg in enumerate(sys.argv):
         if arg=='download' or arg=='dl':
             manager.update()
+            break
         if arg=='play' or arg=='watch':
             manager.play('test')
+            break
         if arg=='watchlist' or arg=='wl':
             manager.watchlist()
+            break
+        if arg=='complete' or arg=='cl':
+            manager.complete(int(sys.argv[i+1])-1)
+            break
         if arg=='test':
             manager.test()
