969e14cc27b19c2e146d64006504ac602abe7d45
PraneethJain <praneethjain005@gmail.com>
2022-06-06 09:51:34 +0530

    Formatted code

--- src/main.py
+++ src/main.py
@@ -1,31 +1,30 @@
 from manager import Manager
 import sys
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     manager = Manager()
-    for i,arg in enumerate(sys.argv):
-        if arg=='download' or arg=='dl':
+    for i, arg in enumerate(sys.argv):
+        if arg == "download" or arg == "dl":
             manager.update()
             break
-        if arg=='play' or arg=='watch':
-            manager.play(int(sys.argv[i+1])-1)
+        if arg == "play" or arg == "watch":
+            manager.play(int(sys.argv[i + 1]) - 1)
             break
-        if arg=='add':
-            for arg in sys.argv[i+1:]:
-                manager.add_show(int(arg)-1)
-            # manager.add_show(int(sys.argv[i+1])-1)
-        if arg=='watchlist' or arg=='wl':
+        if arg == "add":
+            for arg in sys.argv[i + 1 :]:
+                manager.add_show(int(arg) - 1)
+        if arg == "watchlist" or arg == "wl":
             manager.watchlist()
             break
-        if arg=='complete' or arg=='cl':
-            manager.complete(int(sys.argv[i+1])-1)
+        if arg == "complete" or arg == "cl":
+            manager.complete(int(sys.argv[i + 1]) - 1)
             break
-        if arg=='progress':
+        if arg == "progress":
             manager.show_progress()
             break
-        if arg=='series':
+        if arg == "series":
             manager.list_shows()
             break
-        if arg=='test':
+        if arg == "test":
             manager.test()
             break
