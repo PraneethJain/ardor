d6ec54799608a581c16901b9c060e0037bab8cd4
PraneethJain <praneethjain005@gmail.com>
2022-06-16 10:39:56 +0530

    Added multiple selections to download

--- ardor.py
+++ ardor.py
@@ -40,12 +40,67 @@ def selection_menu(options: list[str], transient=False):
             elif key == 13:  # Enter
                 selected = True
                 break
+            elif key == 27:  # Escape
+                break
             if changed:
                 live.update(generate_text(i))
                 live.refresh()
                 changed = False
     if selected:
         return i - 1, options[i - 1]
+
+
+def selection_menu_mutiple(options: list[str], transient=False):
+    def generate_text(selected_indices: list[int], current_index):
+        renderables = []
+        for i, option in enumerate(options):
+            if i in selected_indices:
+                if i == current_index:
+                    renderables.append(f"[magenta bold]>(●) {option}[/magenta bold]")
+                else:
+                    renderables.append(f" [magenta bold](●) {option}[/magenta bold]")
+            elif i == current_index:
+                renderables.append(f"[cyan bold]>(○) {option}[/cyan bold]")
+            else:
+                renderables.append(f" [blue](○) {option}[/blue]")
+        return Group(*renderables)
+
+    selected_indices = []
+    selected_options = []
+    i = 0
+
+    with Live(
+        generate_text(selected_indices, i), auto_refresh=False, transient=transient
+    ) as live:
+        while True:
+            key = ord(getch())
+            changed = False
+            if key == 80:  # Down Arrow
+                i += 1
+                if i == len(options):
+                    i = 0
+                changed = True
+            elif key == 72:  # Up Arrow
+                i -= 1
+                if i == -1:
+                    i = len(options) - 1
+                changed = True
+            elif key == 32:  # Spacebar
+                if i not in selected_indices:
+                    selected_indices.append(i)
+                    selected_options.append(options[i])
+                else:
+                    selected_indices.remove(i)
+                    selected_options.remove(options[i])
+                changed = True
+            elif key == 13:  # Enter
+                return selected_indices, selected_options
+            elif key == 27:  # Escape
+                return
+            if changed:
+                live.update(generate_text(selected_indices, i))
+                live.refresh()
+                changed = False
 
 
 @cli.command()
@@ -86,21 +141,21 @@ def remove(all: bool = False):
 @cli.command()
 def download():
     scraper = Scraper()
-    newly_added = scraper.get_new_episodes()
-    if newly_added:
+    if newly_added := scraper.get_new_episodes():
         print("[bold green]Episodes available for download![/bold green]")
-        i, selection = selection_menu(
-            list(map(lambda x: f"{x['show']} {x['ep']}", newly_added))
-            + ["Download all"],
-            transient=True,
-        )
-        downloader = Downloader()
-        if selection == "Download all":
-            for episode in newly_added:
-                print(downloader.start_torrent(episode))
+        if (
+            (a := selection_menu_mutiple(
+                list(map(lambda x: f"{x['show']} {x['ep']}", newly_added)),
+                transient=True,
+            ))
+            is not None
+        ):
+            selection_indices, _ = a
+            downloader = Downloader()
+            for index in selection_indices:
+                print(downloader.start_torrent(newly_added[index]))
         else:
-            episode = newly_added[i]
-            print(downloader.start_torrent(episode))
+            print("[red bold]No episode selected[red bold]")
     else:
         print("[red bold]No new episodes![/red bold]")
 
